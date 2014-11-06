2b38292d2c4548a0760f62020fcbc6ad4819a8ea
Marc Hayes <marc.hayes.tech@gmail.com>
2014-11-05 23:19:51 -0700

    Laid out script to tear down and rebuild Tables Should work but will troubleshoot once we get our Data Source configured tomorrow.

---
+++ sql/databaseInitialization.sql
@@ -0,0 +1,122 @@
+-- Database initialization for Deep Dive Connect
+--  by Marc Hayes
+
+-- We start with Table tear downs via DROP TABLE IF EXISTS, these are dropped in FK > PK order
+
+DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS topic;
+DROP TABLE IF EXISTS profileCohort;
+DROP TABLE IF EXISTS cohort;
+DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS loginSource;
+DROP TABLE IF EXISTS security;
+
+-- We then build the tables to use in PK > FK order taking care to ensure
+-- PK FK Unique and Link table indices are being accounted for
+
+-- security table
+CREATE TABLE security (
+	securityId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	description VARCHAR(256) NOT NULL,
+	createTopic TINYINT NOT NULL,
+	canEditOther TINYINT NOT NULL,
+	canPromote TINYINT NOT NULL,
+	siteAdmin TINYINT NOT NULL,
+	PRIMARY KEY (securityId),
+	UNIQUE (description)
+);
+
+-- loginSource table
+CREATE TABLE loginSource (
+	loginSourceId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	sourceName VARCHAR(256) NOT NULL,
+	PRIMARY KEY (loginSourceId),
+	UNIQUE (sourceName)
+);
+
+-- user table
+CREATE TABLE user (
+	userId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	email VARCHAR(256) NOT NULL,
+	passwordHash VARCHAR(128),
+	salt VARCHAR(64),
+	authKey VARCHAR(32),
+	securityId INT UNSIGNED NOT NULL,
+	loginSourceId INT UNSIGNED,
+	PRIMARY KEY (userId),
+	UNIQUE (email),
+	INDEX (securityId),
+	INDEX (loginSourceId),
+	FOREIGN KEY (securityId) REFERENCES security(securityId),
+	FOREIGN KEY (loginSourceId) REFERENCES loginSource(loginSourceId)
+);
+
+-- profile table
+CREATE TABLE profile (
+	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	userId INT UNSIGNED NOT NULL,
+	firstName VARCHAR(64) NOT NULL,
+	lastName VARCHAR(64) NOT NULL,
+	middleName VARCHAR(64),
+	location VARCHAR(256),
+	description VARCHAR(4096),
+	profilePicFileName VARCHAR(256),
+	profilePicFileType VARCHAR(16),
+	PRIMARY KEY (profileId),
+	UNIQUE (userId),
+	FOREIGN KEY (userId) REFERENCES user(userId)
+);
+
+-- cohort table
+CREATE TABLE cohort (
+	cohortId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	startDate DATETIME NOT NULL,
+	endDate DATETIME NOT NULL,
+	location VARCHAR(256),
+	description VARCHAR(1024),
+	PRIMARY KEY (cohortId)
+);
+
+-- profileCohort table
+CREATE TABLE profileCohort (
+	profileCohortId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileId INT UNSIGNED NOT NULL,
+	cohortId INT UNSIGNED NOT NULL,
+	role VARCHAR(256) NOT NULL,
+	PRIMARY KEY (profileCohortId),
+	INDEX (profileId),
+	INDEX (cohortId),
+	INDEX (profileId, cohortId),
+	FOREIGN KEY (profileId) REFERENCES profile(profileId),
+	FOREIGN KEY (cohortId) REFERENCES cohort(cohortId)
+);
+
+-- topic table
+CREATE TABLE topic (
+	topicId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileId INT UNSIGNED NOT NULL,
+	creationDate DATETIME NOT NULL,
+	topicSubject VARCHAR(256) NOT NULL,
+	topicBody VARCHAR(4096) NOT NULL,
+	PRIMARY KEY (topicId),
+	INDEX (profileId),
+	INDEX (creationDate),
+	INDEX (topicSubject),
+	FOREIGN KEY (profileId) REFERENCES profile(profileId)
+);
+
+-- comment
+CREATE TABLE comment (
+	commentId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	topicId INT UNSIGNED NOT NULL,
+	profileId INT UNSIGNED NOT NULL,
+	commentSubject VARCHAR(256) NULL,
+	commentBody VARCHAR(1024) NULL,
+	PRIMARY KEY (commentId),
+	INDEX (topicId),
+	INDEX (profileId),
+	INDEX (topicId, profileId),
+	FOREIGN KEY (profileId) REFERENCES profile(profileId),
+	FOREIGN KEY (topicId) REFERENCES topic(topicId)
+);
