43819935f63d004d8a4b8972597ecdff56e7fa14
Steven-Chavez <schavez256@yahoo.com>
2014-11-06 15:30:57 -0700

    Added User state https://app.asana.com/0/19248945971906/19255978551020

--- php/user.php
+++ php/user.php
@@ -1,7 +1,45 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Steven
- * Date: 11/6/2014
- * Time: 3:02 PM
- */ 
+ * mySQL Enabled User
+ *
+ * This is a mySQl enabled container for User authentication.
+ *
+ * @author Steven Chavez <schavez256@yahoo.com>
+ * @see Profile
+ */
+class User{
+	/**
+	 * user id for User; primary key
+	 */
+	private $userId;
+	/**
+	 * email for User; unique field
+	 */
+	private $email;
+	/**
+	 * SHA512 PBKDF2 hash of the password
+	 */
+	private $passwordHash;
+	/**
+	 * salt used in the PBKDF2 hash
+	 */
+	private $salt;
+	/**
+	 * authentication token used in a new account and password resets
+	 */
+	private $authKey;
+	/**
+	 * securityId identifies the users security clearances
+	 */
+	private $securityId;
+	/**
+	 * identifies whether user is logging in through site or external options
+	 */
+	private $loginSourceId;
+
+	/**
+	 * constructor for User
+	 *
+	 */
+}
+?>
