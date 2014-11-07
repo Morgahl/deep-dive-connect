9ce0e8f68f052b69392a8f95df4d43245de0987e
Joseph Bottone <bottone.joseph@gmail.com>
2014-11-06 17:10:47 -0700

    I created the security class, the constructors, and a try and catch.
    https://app.asana.com/0/19128794071958/19599640169229

--- php/securityClass.php
+++ php/securityClass.php
@@ -1,3 +1,34 @@
 <?php
 
+class SecurityClass
+{
+	// assigns the primary key
+	private $securityId;
+	// names the description
+	private $description;
+	// name of createTopic
+	private $createTopic;
+	// name of canEditOther
+	private $canEditOther;
+	// name of canPromote
+	private $canPromote;
+	// name of siteAdmin
+	private $siteAdmin;
+
+	// here is the constructor
+	public function __construct($newSecurityId, $newDescription, $newCreateTopic, $newCanEditOther, $newCanPromote,
+	$newSiteAdmin) {
+		try {
+			$this->setsecurityId($newSecurityId);
+			$this->setdescription($newDescription);
+			$this->setcreateTopic($newCreateTopic)
+			$this->setcanEditOther($newCanEditOther);
+			$this->setcanPromote($newCanPromote);
+			$this->setsiteAdmin($newSiteAdmin);
+		} catch{
+		// ToDo: implement catchblock
+		}
+	}
+
+
 ?>
