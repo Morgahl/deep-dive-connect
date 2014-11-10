2f8eda77e78901bd254f455ecf6ba0ee878f9120
Gerry Medrano <lux808@hotmail.com>
2014-11-10 13:21:04 -0700

    Testing asana  and updated code for this class.  https://app.asana.com/0/dashboard/19129992621201

--- php/cohort.php
+++ php/cohort.php
@@ -1,7 +1,10 @@
 <?php
 /**
- * Created by PhpStorm.
- * User:  GM This is a hot mess!
+ * This is a container for class Cohort
+ * This class will identify elements of Cohort class to link to
+ * Deep Dive Coders Alumni
+ * Created by Gerardo Medrano, adapted from template by Dylan M
+ * User:  Deep Dive Connect; GM This is a hot mess!
  * Date: 11/6/2014
  * Time: 3:16 PM
  */
@@ -9,19 +12,19 @@
 //Wrote Cohort class below; primary key is the cohortId;
 class Cohort
 {
-
+   //this is a primary key
    private $cohortID;
-
+   //this
    private $startDate;
-
+   //comment!
    private $endDate;
-
+   //comment!
    private $location;
-
+   //comment!
    private $description;
 
 
-//** constructor below */
+   //** constructor below for cohort class */
 
    public function __construct($newCohortID, $newStartDate, $newEndDate, $newLocation, $newDescription)
    {
@@ -32,7 +35,7 @@ class Cohort
          $this->setLocation($newLocation);
          $this->setDescription($newDescription);
       } catch(UnexpectedValueException $unexpectedValue) {
-         //comment!
+         //comment!  3d
          throw(new UnexpectedValueException("Unable to associate with Cohort", 0, $unexpectedValue));
          //comment!
       } catch(RangeException $range) {
@@ -41,7 +44,7 @@ class Cohort
       }
    }
 
-//gets value of CohortID
+   //gets value of CohortID
    public function setCohortID($newCohortID)
    {
       // zeroth, set allow the CohortID to be null if a new object
@@ -50,22 +53,173 @@ class Cohort
          return;
       }
 
-//FilterVar here
+      //FilterVar here
       if(filter_var($newCohortID, FILTER_VALIDATE_INT) === false) {
          throw(new UnexpectedValueException("Cohort ID $newCohortID is not numeric"));
       }
 
-//convert CohortID to an interger
+      //convert CohortID to an integer
       $newCohortID = intval($newCohortID);
       if($newCohortID <= 0) {
          throw(new RangeException("Cohort ID $newCohortID is not positive"));
       }
 
-//remove CohortID from quarantine below
+      //remove CohortID from quarantine below
       $this->CohortID = $newCohortID;
    }
 
-//get value of CohortID return string--what's name of field/output, and string??
+   //gets value of StartDate
+   public function setStartDate($newStartDate)
+   {
+      // zeroth, set allow the StartDate to be null if a new object
+      if($newStartDate === null) {
+         $this->startDate = null;
+         return;
+      }
+
+      //FilterVar here for StartDate
+      if(filter_var($newStartDate, FILTER_VALIDATE_INT) === false) {
+         throw(new UnexpectedValueException("Start Date $newStartDate is not numeric"));
+      }
+
+      //convert StartDate to an integer
+      $newStartDate = intval($newStartDate);
+      if($newStartDate <= 0) {
+         throw(new RangeException("Start Date $newStartDate is not positive"));
+      }
+
+      //remove StartDate from quarantine below
+      $this->StartDate = $newStartDate;
+
+   }
+
+//gets value of EndDate
+   public function setEndDate($newEndDate)
+   {
+      // zeroth, set allow the EndDate to be null if a new object
+      if($newEndDate === null) {
+         $this->EndDate = null;
+         return;
+      }
+
+      //FilterVar here for EndDate
+      if(filter_var($newEndDate, FILTER_VALIDATE_INT) === false) {
+         throw(new UnexpectedValueException("End Date $newEndDate is not numeric"));
+      }
+
+      //convert EndDate to an integer
+      $newEndDate = intval($newEndDate);
+      if($newEndDate <= 0) {
+         throw(new RangeException("End Date $newEndDate is not positive"));
+      }
+
+      //remove EndDate from quarantine below
+      $this->EndDate = $newEndDate;
+
+   }
+   //gets value of Location
+   public function setLocation($newLocation)
+   {
+      // zeroth, set allow the Location to be null if a new object
+      if($newLocation === null) {
+         $this->Location = null;
+         return;
+      }
+
+      //FilterVar here for Location
+      if(filter_var($newLocation, FILTER_VALIDATE_INT) === false) {
+         throw(new UnexpectedValueException("Location $newLocation is not numeric"));
+      }
+
+      //convert Location to an integer
+      $newLocation = intval($newLocation);
+      if($newLocation <= 0) {
+         throw(new RangeException("Location $newLocation is not positive"));
+      }
+
+      //remove Location from quarantine below
+      $this->Location = $newLocation;
+
+   }
+
+
+   //gets value of Description
+   public function setDescription($newDescription)  {
+      // zeroth, set allow the Location to be null if a new object
+      if($newDescription === null) {
+         $this->Description = null;
+         return;
+      }
+
+      //FilterVar here for Description --- What should be used for throw verbiage?
+      if(filter_var($newDescription, FILTER_VALIDATE_INT) === false) {
+         throw(new UnexpectedValueException("Description $newDescription is not Valid"));
+      }
+
+      //convert Description to an integer
+      $newDescription = intval($newDescription);
+      if($newDescription <= 0) {
+         throw(new RangeException("Location $newDescription is not positive"));
+      }
+
+      //remove Description from quarantine below
+      $this->Description = $newDescription;
+
+      }
+
+   /**
+    *Insert Profile Cohort to mySQL
+    * @paran mysqli_sql_exception when mySql related errors occur
+    *
+    **/
+   public function insert(&$mysqli) {
+
+
+      if(gettype(mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
+         throw(new mysqli_sql_exception("input is not a mysqli object"));
+      }
+
+      //enforce the Profile Cohort ID is null
+
+      if($this->profileCohortId !== null) {
+         throw(new mysqli_sql_exception("input is not a mysqli object"));
+      }
+
+
+      //create a query template Profile Cohort ID
+      //To do: Insert Field names for $query
+      $query      =  "INSERT INTO profileCohortId (XXXXPROFILeCOHORtFIELDsHEREXXX) VALUES?";
+      $statement  =  $mysqli ->prepare($query);
+         if($statement === false) {
+            throw(new mysqli_sql_exception("Unable to prepare statement"));
+      }
+
+      //bind the member variables to place holders in template
+      $wasClean = $statement->bind_param("ssd", $this->XXXXFIELD1,$this->XXXXFIELD1,etc);
+         if($wasClean === false) {
+            throw(new mysqli_sql_exception("Unable to bind parameters"));
+      }
+
+      //execute the statement
+         if($statement->execute() === false) {
+            throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
+      }
+
+      // update the null Profile Cohort Id with Mysql output
+      $this->profileCohortId = $mysqli->insert_id;
+
+
+
+
+
+
+
+
+
+
+
+
+//For below see Mark's function
    public function getStartDate()
    {
       return ($this->startDate);
--- php/profileCohort.php
+++ php/profileCohort.php
@@ -1,9 +1,358 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: gmedrano
- * Date: 11/6/2014
- * Time: 3:14 PM
- */
+ * User: gnosis gnosis
+ * Date: 11/7/2014
+ * Time: 10:44 AM
+ **/
 
-?>
+
+
+class profileCohort {
+   //profileCohortID is the private key
+   private $profileCohortId;
+
+   //names cohortId for alum
+   private $cohortId;
+
+   //names ProfileId for alum
+   private $profileId;
+
+   //names location of alum
+   private $location;
+
+   //names role of alum
+   private $role;
+   /**
+    * constructor for ProfileCohort below
+    * "@param" comments need to be entered
+    *
+    *
+    */
+   public function __construct($newProfileCohortId, $newProfileId, $newCohortId, $newLocation, $newRole)
+   {
+      try {
+         $this->setProfileCohortId($newProfileCohortId);
+         $this->setProfileId($newProfileId);
+         $this->setCohortId($newCohortId);
+         $this->setLocation($newLocation);
+         $this->setRole($newRole);
+
+      } catch(UnexpectedValueException $unexpectedValue) {
+         // rethrow to the seller
+         throw(new UnexpectedValueException("Unable to construct Profile Cohort", 0, $unexpectedValue));
+
+      } catch(RangeException $range) {
+         // rethrow to the caller
+         throw(new RangeException("Unable to construct Profile Cohort", 0, $range));
+      }
+   }
+
+   //gets value of ProfileCohortId
+   //returns mixed ProfileCohortId
+   public function getProfileCohortId() {
+      return ($this->profileCohortId);
+   }
+   //set ProfileCohortId
+   /**
+    * @param $newProfileCohortId
+    */
+   public function setProfileCohortId($newProfileCohortId) {
+      if($newProfileCohortId === null) {
+         $this->profileCohortId = null;
+         return;
+      }
+      //filter_var for ProfileCohortId
+      if(filter_var($newProfileCohortId, FILTER_VALIDATE_INT) === false) {
+         throw(new UnexpectedValueException("Profile Cohort Id $newProfileCohortId is not numeric"));
+      }
+      //convert the ProfileCohortId to an integer and enforce it is positive
+      $newProfileCohortId = intval($newProfileCohortId);
+      if($newProfileCohortId <= 0) {
+         throw(new RangeException("Profile Cohort Id $newProfileCohortId is not positive"));
+      }
+      //take the Profile Cohort Id out of quarantine
+      $this->profileCohortId = $newProfileCohortId;
+   }
+
+
+   //gets value of ProfileId
+   //returns mixed ProfileId
+   public function getProfileId() {
+      return ($this->profileId);
+   }
+   /**set the value of ProfileId**/
+   public function setProfileId($newProfileId) {
+      if($newProfileId === null) {
+         $this->profileId = null;
+         return;
+      }
+      //filter_var for ProfileId
+      if(filter_var($newProfileId, FILTER_VALIDATE_INT) === false) {
+         throw(new UnexpectedValueException("Profile Id $newProfileId is not numeric"));
+      }
+      //convert the ProfileCohortId to an integer and enforce it is positive
+      $newProfileId = intval($newProfileId);
+      if($newProfileId <= 0) {
+         throw(new RangeException("Profile Id $newProfileId is not positive"));
+      }
+      //take the Profile Cohort Id out of quarantine
+      $this->profileId = $newProfileId;
+   }
+
+
+   //gets value of CohortId
+   //returns mixed CohortId
+   public function getCohortId() {
+      return ($this->CohortId);
+   }
+
+   //set CohortId
+   public function setCohortId($newCohortId)
+   {
+      if($newCohortId === null) {
+         $this->CohortId = null;
+         return;
+      }
+      //filter_var for CohortId
+      if(filter_var($newCohortId, FILTER_VALIDATE_INT) === false) {
+         throw(new UnexpectedValueException("Cohort Id $newCohortId is not numeric"));
+      }
+      //convert the CohortId to an integer and enforce it is positive
+      $newCohortId = intval($newCohortId);
+      if($newCohortId <= 0) {
+         throw(new RangeException("Profile Id $newCohortId is not positive"));
+         //take the Profile Cohort Id out of quarantine
+         $this->CohortId = $newCohortId;
+      }
+   }
+
+   //Get and Set for location
+   public function getLocation()
+   {
+      return ($this->location);
+   }
+
+   public function setLocation($newLocation)
+   {
+      // filter the location as a generic string
+      $newLocation = trim($newLocation);
+      $newLocation = filter_var($newLocation, FILTER_SANITIZE_STRING);
+
+      // then just take the location out of quarantine
+      $this->Location = $newLocation;
+   }
+
+   //Get and Set for role
+   public function getRole(){
+      return ($this->role);
+   }
+   public function setRole($newRole)
+   {
+      $newRole = trim($newRole);
+      // filter the role as a generic string
+      $newRole = filter_var($newRole, FILTER_SANITIZE_STRING);
+
+      // then just take the role out of quarantine
+
+      $this->role = $newRole;
+
+   }
+   /**
+    *Insert Profile Cohort to mySQL
+    * @paran mysqli_sql_exception when mySql related errors occur
+    *
+    **/
+   public function insert(&$mysqli) {
+   }
+
+if(gettype(mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
+throw(new mysqli_sql_exception("input is not a mysqli object"));
+}
+
+//enforce the Profile Cohort ID is null
+
+if($this->profileCohortId !== null) {
+   throw(new mysqli_sql_exception("input is not a mysqli object"));
+}
+
+
+//create a query template Profile Cohort ID
+$query      =  "INSERT INTO profileCohortId (XXXXPROFILeCOHORtFIELDsHEREXXX) VALUES?";
+$statement  =  $mysqli ->prepare($query);
+if($statement === false) {
+   throw(new mysqli_sql_exception("Unable to prepare statement"));
+}
+
+//bind the member variables to place holders in template
+$wasClean = $statement->bind_param("ssd", $this->XXXXFIELD1,$this->XXXXFIELD1,etc);
+if($wasClean === false) {
+   throw(new mysqli_sql_exception("Unable to bind parameters"));
+}
+
+//execute the statement
+if($statement->execute() === false) {
+   throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
+}
+
+// update the null Profile Cohort Id with Mysql output
+$this->profileCohortId = $mysqli->insert_id;
+
+
+}
+
+/**
+ * deletes CohortId from mySQL
+ *
+ * @param resource $mysqli pointer to mySQL connection, by reference
+ * @throws mysqli_sql_exception when mySQL related errors occur
+ *
+ **/
+public function insert(&$mysqli) {
+}
+
+if(gettype(mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
+   throw(new mysqli_sql_exception("input is not a mysqli object"));
+}
+
+//enforce Cohort ID is null
+
+if($this->CohortId !== null) {
+   throw(new mysqli_sql_exception("input is not a mysqli object"));
+}
+
+
+//create a query template for Cohort ID
+$query      =  "INSERT INTO CohortId (XXXXPROFILeCOHORtFIELDsHEREXXX) VALUES?";
+$statement  =  $mysqli ->prepare($query);
+if($statement === false) {
+   throw(new mysqli_sql_exception("Unable to prepare statement"));
+}
+
+//bind the member variables to place holders in template
+$wasClean = $statement->bind_param("ssd", $this->XXXXFIELD1,$this->XXXXFIELD1,etc);
+if($wasClean === false) {
+   throw(new mysqli_sql_exception("Unable to bind parameters"));
+
+}
+
+//execute the statement
+if($statement->execute() === false) {
+   throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
+}
+
+// update the null Cohort Id with Mysql output
+$this->CohortId = $mysqli->insert_id;
+
+
+/**
+ * deletes CohortId from mySQL
+ *
+ * @param resource $mysqli pointer to mySQL connection, by reference
+ * @throws mysqli_sql_exception when mySQL related errors occur
+ *
+ **/
+public function insert(&$mysqli) {
+}
+
+if(gettype(mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
+   throw(new mysqli_sql_exception("input is not a mysqli object"));
+}
+
+//enforce Cohort ID is null
+
+if($this->CohortId !== null) {
+   throw(new mysqli_sql_exception("input is not a mysqli object"));
+}
+
+
+//create a query template for Cohort ID
+$query      =  "INSERT INTO CohortId (XXXXPROFILeCOHORtFIELDsHEREXXX) VALUES?";
+$statement  =  $mysqli ->prepare($query);
+if($statement === false) {
+   throw(new mysqli_sql_exception("Unable to prepare statement"));
+}
+
+//bind the member variables to place holders in template
+$wasClean = $statement->bind_param("ssd", $this->XXXXFIELD1,$this->XXXXFIELD1,etc);
+if($wasClean === false) {
+   throw(new mysqli_sql_exception("Unable to bind parameters"));
+
+}
+
+//execute the statement
+if($statement->execute() === false) {
+   throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
+}
+
+// update the null Cohort Id with Mysql output
+$this->CohortId = $mysqli->insert_id;
+
+
+/**
+ * deletes ProfileId from mySQL
+ *
+ * @param resource $mysqli pointer to mySQL connection, by reference
+ * @throws mysqli_sql_exception when mySQL related errors occur
+ *
+ **/
+public function insert(&$mysqli) {
+}
+
+if(gettype(mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
+   throw(new mysqli_sql_exception("input is not a mysqli object"));
+}
+
+//enforce ProfileID is null
+
+if($this->ProfileId !== null) {
+   throw(new mysqli_sql_exception("input is not a mysqli object"));
+}
+
+
+//create a query template for Profile ID
+$query      =  "INSERT INTO ProfileId (XXXXPROFILeCOHORtFIELDsHEREXXX) VALUES?";
+$statement  =  $mysqli ->prepare($query);
+if($statement === false) {
+   throw(new mysqli_sql_exception("Unable to prepare statement"));
+}
+
+//bind the member variables to place holders in template
+$wasClean = $statement->bind_param("ssd", $this->XXXXFIELD1,$this->XXXXFIELD1,etc);
+if($wasClean === false) {
+   throw(new mysqli_sql_exception("Unable to bind parameters"));
+
+}
+
+//execute the statement
+if($statement->execute() === false) {
+   throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
+}
+
+// update the null Profile Id with Mysql output
+$this->ProfileId = $mysqli->insert_id;
+
+
+/**
+ * gets the Product by name
+ *
+ * @param resource $mysqli pointer to mySQL connection, by reference
+ * @param string $productName name to search for
+ * @return mixed array of Products found, Product found, or null if not found
+ * @throws mysqli_sql_exception when mySQL related errors occur
+ **/
+public static function getCohortByName(&$mysqli, $CohortName) {
+   // handle degenerate cases
+   if(gettype($mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
+      throw(new mysqli_sql_exception("input is not a mysqli object"));
+   }
+
+   // sanitize the description before searching
+   $cohortName = trim($cohortName);
+   $productName = filter_var($productName, FILTER_SANITIZE_STRING);
+
+//primary key should allow a null
+//set the value at the end
+//allow null
+//filter string from Topic table
+//data sanitize
