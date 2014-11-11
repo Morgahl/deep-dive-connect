04d7627fa2caed6b75206104d5517791f278f8fe
Gerry Medrano <lux808@hotmail.com>
2014-11-10 21:09:07 -0700

    https://app.asana.com/0/dashboard/19129992621201 Added DateTime  object fragments

--- php/cohort.php
+++ php/cohort.php
@@ -1,31 +1,41 @@
 <?php
 /**
  * This is a container for class Cohort
- * This class will identify elements of Cohort class to link to
- * Deep Dive Coders Alumni
- * Created by Gerardo Medrano, adapted from template by Dylan M
- * User:  Deep Dive Connect; GM This is a hot mess!
+ *
+ * This class will identify elements of Cohort class to link to Deep Dive Coders Alumni Site
+ * Created by Gerardo Medrano, adapted from template by Dylan McDonald
+ *
  * Date: 11/6/2014
  * Time: 3:16 PM
- */
+ **/
 
-//Wrote Cohort class below; primary key is the cohortId;
-class Cohort
-{
-   //this is a primary key
+//**Wrote Cohort class below; primary key is the cohortId;
+class Cohort {
+   // CohortID identifies primary key for Cohort
    private $cohortID;
-   //this
+
+   // Start date from date string
    private $startDate;
-   //comment!
+
+   // End Date from date string
    private $endDate;
-   //comment!
+
+   //location of alum; string includes present city and state info
    private $location;
-   //comment!
+
+   //open-ended thread escribing the alumnus' profile, job status, etc.
    private $description;
 
 
-   //** constructor below for cohort class */
-
+   /** constructor below for Cohort class
+    * @param string $newCohortId Cohort Id (or null if new object)
+    * @param string $newStartDate Start Date of Cohort
+    * @param string $newEndDate End Date of Cohort
+    * @param string $newLocation Location of Cohort
+    * @param string $newDescription Description of Alumnus' activities
+    * @throws UnexpectedValueException when a parameter is wront type
+    * @throws RangeException when a parameter is invalid
+    **/
    public function __construct($newCohortID, $newStartDate, $newEndDate, $newLocation, $newDescription)
    {
       try {
@@ -35,16 +45,32 @@ class Cohort
          $this->setLocation($newLocation);
          $this->setDescription($newDescription);
       } catch(UnexpectedValueException $unexpectedValue) {
-         //comment!  3d
+
+   //creates throw statement
          throw(new UnexpectedValueException("Unable to associate with Cohort", 0, $unexpectedValue));
-         //comment!
+   //catch statement
       } catch(RangeException $range) {
          //rethrow to the caller
          throw(new RangeException("Unable to associate with Cohort", 0, $range));
       }
    }
 
-   //gets value of CohortID
+/** gets the value of Cohort Id
+ *
+ * @return mixed Cohort Id (or null if new object)
+ **/
+
+   public function getCohortId() {
+      return ($this->cohortID);
+
+}
+
+/** sets the value of Cohort Id
+ *
+ * @param mixed $newCohortId (or null if new object)
+ * @throws UnexpectedValueException if not an integer or null
+ * @throws RangeException if Cohort Id isn't a positive
+ **/
    public function setCohortID($newCohortID)
    {
       // zeroth, set allow the CohortID to be null if a new object
@@ -53,39 +79,59 @@ class Cohort
          return;
       }
 
-      //FilterVar here
+      //first, filterVar ensures the Cohort Id is an integer
       if(filter_var($newCohortID, FILTER_VALIDATE_INT) === false) {
          throw(new UnexpectedValueException("Cohort ID $newCohortID is not numeric"));
       }
 
-      //convert CohortID to an integer
+      //second, convert CohortID to an integer and enforce it's positive
       $newCohortID = intval($newCohortID);
       if($newCohortID <= 0) {
          throw(new RangeException("Cohort ID $newCohortID is not positive"));
       }
 
-      //remove CohortID from quarantine below
+      //finally remove CohortID from quarantine and assign it
       $this->CohortID = $newCohortID;
    }
 
-   //gets value of StartDate
-   public function setStartDate($newStartDate)
-   {
-      // zeroth, set allow the StartDate to be null if a new object
-      if($newStartDate === null) {
-         $this->startDate = null;
+   /**
+    * gets the value of StartDate of Cohort
+    **/
+    public function getStartDate () {
+         return($this->startDate);
+   }
+
+   /** sets the value of StartDate of Cohort
+   *
+   * @param mixed $StartDate object or string with date created
+   * @throws exception if date is not a valid date
+   *
+   **/
+   public function setStartDate($newStartDate) {
+      //zeroth, allow the date to be null if a new object
+      if ($newStartDate === null) {
+            $this->startDate = null;
+            return;
+      }
+
+      // zeroth, allow a DateTime object to be directly assigned
+      if(gettype($newStartDate) === "object" && get_class($newStartDate) === "DateTime") {
+         $this->startDate = $newStartDate;
          return;
       }
 
-      //FilterVar here for StartDate
-      if(filter_var($newStartDate, FILTER_VALIDATE_INT) === false) {
-         throw(new UnexpectedValueException("Start Date $newStartDate is not numeric"));
+      // treat the StartDate as a mySQL date string
+      $newDateCreated = trim($newStartDate);
+      if((preg_match("/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})$/", $newStartDate, $matches)) !== 1) {
+         throw(new RangeException("Your Start Date is not a valid date"));
       }
 
-      //convert StartDate to an integer
-      $newStartDate = intval($newStartDate);
-      if($newStartDate <= 0) {
-         throw(new RangeException("Start Date $newStartDate is not positive"));
+      // verify the date is really a valid calendar date
+      $year  = intval($matches[1]);
+      $month = intval($matches[2]);
+      $day   = intval($matches[3]);
+      if(checkdate($month, $day, $year) === false) {
+         throw(new RangeException(" $newStartDate is not a Gregorian date"));
       }
 
       //remove StartDate from quarantine below
@@ -93,79 +139,148 @@ class Cohort
 
    }
 
-//gets value of EndDate
-   public function setEndDate($newEndDate)
-   {
-      // zeroth, set allow the EndDate to be null if a new object
-      if($newEndDate === null) {
-         $this->EndDate = null;
-         return;
-      }
+   /**
+   * gets the value of EndDate of Cohort
+   **/
+      public function getEndDate () {
+         return($this->EndDate);
+}
 
-      //FilterVar here for EndDate
-      if(filter_var($newEndDate, FILTER_VALIDATE_INT) === false) {
-         throw(new UnexpectedValueException("End Date $newEndDate is not numeric"));
-      }
-
-      //convert EndDate to an integer
-      $newEndDate = intval($newEndDate);
-      if($newEndDate <= 0) {
-         throw(new RangeException("End Date $newEndDate is not positive"));
-      }
-
-      //remove EndDate from quarantine below
-      $this->EndDate = $newEndDate;
-
+/** sets the value of EndDate of Cohort
+ *
+ * @param mixed $EndDate object or string with date created
+ * @throws exception if date is not a valid date
+ *
+ **/
+public function setEndDate($newEndDate) {
+   //zeroth, allow the date to be null if a new object
+   if ($newEndDate === null) {
+      $this->EndDate = null;
+      return;
    }
-   //gets value of Location
-   public function setLocation($newLocation)
-   {
-      // zeroth, set allow the Location to be null if a new object
-      if($newLocation === null) {
-         $this->Location = null;
-         return;
+
+   // zeroth, allow a DateTime object to be directly assigned
+   if(gettype($newEndDate) === "object" && get_class($newEndDate) === "DateTime") {
+      $this->startDate = $newEndDate;
+      return;
+   }
+
+   // treat the StartDate as a mySQL date string
+   $newDateCreated = trim($newEndDate);
+   if((preg_match("/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})$/", $newEndDate, $matches)) !== 1) {
+      throw(new RangeException("Your Start Date is not a valid date"));
+   }
+
+   // verify the date is really a valid calendar date
+   $year  = intval($matches[1]);
+   $month = intval($matches[2]);
+   $day   = intval($matches[3]);
+   if(checkdate($month, $day, $year) === false) {
+      throw(new RangeException(" End Date is not a Gregorian date"));
+   }
+
+   //remove StartDate from quarantine below
+   $this->EndDate = $newEndDate;
+
+}
+
+   /**
+    * updates this Signup in mySQL
+    *
+    * @param resource $mysqli pointer to mySQL connection, by reference
+    * @throws mysqli_sql_exception when mySQL related errors occur
+    **/
+   public function update(&$mysqli) {
+      // handle degenerate cases
+      if(gettype($mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
+         throw(new mysqli_sql_exception("input is not a mysqli object"));
       }
 
-      //FilterVar here for Location
-      if(filter_var($newLocation, FILTER_VALIDATE_INT) === false) {
-         throw(new UnexpectedValueException("Location $newLocation is not numeric"));
+      // enforce the signupId is not null (i.e., don't update a signup that hasn't been inserted)
+      if($this->signupId === null) {
+         throw(new mysqli_sql_exception("Unable to update a signup that does not exist"));
       }
 
-      //convert Location to an integer
-      $newLocation = intval($newLocation);
-      if($newLocation <= 0) {
-         throw(new RangeException("Location $newLocation is not positive"));
+      // convert dates to strings
+      if($this->dateConfirmed === null) {
+         $dateConfirmed = null;
+      } else {
+         $dateConfirmed = $this->dateConfirmed->format("Y-d-m H:i:s");
       }
+      if($this->dateCreated === null) {
+         $dateCreated = null;
+      } else {
+         $dateCreated = $this->dateCreated->format("Y-d-m H:i:s");
+      }
+
+      // create query template
+      $query     = "UPDATE signup SET firstName = ?, lastName = ?, email = ?, authToken = ?, dateCreated = ?, dateConfirmed = ? WHERE signupId = ?";
+      $statement = $mysqli->prepare($query);
+      if($statement === false) {
+         throw(new mysqli_sql_exception("Unable to prepare statement"));
+      }
+
+      // bind the member variables to the place holders in the template
+      $wasClean = $statement->bind_param("ssssssi",   $this->firstName, $this->lastName, $this->email,
+         $this->authToken, $dateCreated, $dateConfirmed, $this->signupId);
+      if($wasClean === false) {
+         throw(new mysqli_sql_exception("Unable to bind parameters"));
+      }
+
+      // execute the statement
+      if($statement->execute() === false) {
+         throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
+      }
+   }
+
+/**
+ * gets the value of Location
+ *
+ * @return string Location
+ **/
+   public function getLocation() {
+      return ($this->Location);
+   }
+
+/**
+ * sets the value of Location
+ *
+ * @return string Location
+ **/
+   public function setLocation($newLocation) {
+      // filter the location as a generic string
+      $newLocation = trim($newLocation);
+      $newLocation = filter_var($newLocation, FILTER_SANITIZE_STRING);
 
       //remove Location from quarantine below
       $this->Location = $newLocation;
 
    }
 
+   /**
+    * gets the value of Description
+    *
+    * @return string Description
+    **/
 
-   //gets value of Description
-   public function setDescription($newDescription)  {
-      // zeroth, set allow the Location to be null if a new object
-      if($newDescription === null) {
-         $this->Description = null;
-         return;
-      }
+   public function getDescription() {
+      return ($this->description);
+   }
 
-      //FilterVar here for Description --- What should be used for throw verbiage?
-      if(filter_var($newDescription, FILTER_VALIDATE_INT) === false) {
-         throw(new UnexpectedValueException("Description $newDescription is not Valid"));
-      }
+/**
+ * sets the value of Description
+ *
+ * @return string Description
+ **/
+   public function setDescription($newDescription) {
+   // filter the location as a generic string
+      $newLocation = trim($newDescription);
+      $newLocation = filter_var($newDescription, FILTER_SANITIZE_STRING);
 
-      //convert Description to an integer
-      $newDescription = intval($newDescription);
-      if($newDescription <= 0) {
-         throw(new RangeException("Location $newDescription is not positive"));
-      }
+   //remove Description from quarantine below
+   $this->Description = $newDescription;
 
-      //remove Description from quarantine below
-      $this->Description = $newDescription;
 
-      }
 
    /**
     *Insert Profile Cohort to mySQL
@@ -205,25 +320,10 @@ class Cohort
             throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
       }
 
-      // update the null Profile Cohort Id with Mysql output
-      $this->profileCohortId = $mysqli->insert_id;
-
-
-
-
-
-
-
-
-
-
-
-
-//For below see Mark's function
-   public function getStartDate()
-   {
-      return ($this->startDate);
-
    }
 }
+
+
+
+
 ?>
