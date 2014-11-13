9e9908ae7874d91e7a41b418a6a9b17a7bb3f842
Joseph Bottone <bottone.joseph@gmail.com>
2014-11-12 17:00:55 -0700

    ((Unit test for securityClass)) Half of code is finished.
    https://app.asana.com/0/19128794071958/19599640169229

--- test/securityClass-test.php
+++ test/securityClass-test.php
@@ -6,6 +6,9 @@
 // thundermedia.com
 // Unit test for SecurityClass
 
+
+// require mysqli connection object
+require_once("/etc/apache2/capstone-mysql/ddconnect.php");
 // first require the SimpleTest framework
 require_once("/usr/lib/php5/simpletest/autorun.php");
 
@@ -17,42 +20,33 @@ class UserTest extends UnitTestCase {
 	// variable to hold the mySQL connection
 	private $mysqli = null;
 	// variable to hold the test database row
-	private $user   = null;
+	private $security   = null;
 
 	// a few "global" variables for creating test data
-	private $SECURITYID      = "unit-test@example.net";
-	private $DESCRIPTION   = "ChedGeek5";
-	private $ISDEFAULT       = null;
-	private $CREATETOPIC = null;
-	private $CANEDITOTHER       = null;
-	private $CANPROMOTE = null;
-	private $SITEADMIN       = null;
+	private $SECURITYID = null;
+	private $DESCRIPTION = "ChedGeek5";
+	private $ISDEFAULT       = 0;
+	private $CREATETOPIC = 1;
+	private $CANEDITOTHER       = 1;
+	private $CANPROMOTE = 1;
+	private $SITEADMIN       = 1;
 
 	// setUp() is a method that is run before each test
 	// here, we use it to connect to mySQL and to calculate the salt, hash, and authenticationToken
 	public function setUp() {
 		// connect to mySQL
-		mysqli_report(MYSQLI_REPORT_STRICT);
-		$this->mysqli = new mysqli("/etc/apache2/capstone-mysql/ddconnect.php"");
 
-		// randomize the salt, hash, and authentication token
-		$this->SALT       = bin2hex(openssl_random_pseudo_bytes(32));
-		$this->AUTH_TOKEN = bin2hex(openssl_random_pseudo_bytes(16));
-		$this->HASH       = hash_pbkdf2("sha512", $this->PASSWORD, $this->SALT, 2048, 128);
+		$this->mysqli =  MysqliConfiguration::getMysqli();
 	}
 
 	// tearDown() is a method that is run after each test
 	// here, we use it to delete the test record and disconnect from mySQL
 	public function tearDown() {
 		// delete the user if we can
-		if($this->user !== null) {
-			$this->user->delete($this->mysqli);
-			$this->user = null;
+		if($this->security !== null) {
+			$this->security->delete($this->mysqli);
+			$this->security = null;
 		}
-
-		// DEACTTIVATED disconnect from mySQL
-		// if($this->mysqli !== null) {
-		// $this->mysqli->close();
 
 	}
 
@@ -62,91 +56,18 @@ class UserTest extends UnitTestCase {
 		$this->assertNotNull($this->mysqli);
 
 		// second, create a user to post to mySQL
-		$this->user = new User(null, $this->EMAIL, $this->HASH, $this->SALT, $this->AUTH_TOKEN);
+		$this->security = new SecurityClass(null, $this->DESCRIPTION, $this->ISDEFAULT, $this->CREATETOPIC, $this->CANEDITOTHER, $this->CANPROMOTE, $this->SITEADMIN);
 
 		// third, insert the user to mySQL
-		$this->user->insert($this->mysqli);
+		$this->security->insert($this->mysqli);
 
 		// finally, compare the fields
-		$this->assertNotNull($this->user->getUserId());
-		$this->assertTrue($this->user->getUserId() > 0);
-		$this->assertIdentical($this->user->getEmail(),               $this->EMAIL);
-		$this->assertIdentical($this->user->getPassword(),            $this->HASH);
-		$this->assertIdentical($this->user->getSalt(),                $this->SALT);
-		$this->assertIdentical($this->user->getAuthenticationToken(), $this->AUTH_TOKEN);
+		$this->assertNotNull($this->security->getSecurityId());
+		$this->assertTrue($this->security->getSecurityId() > 0);
+		$this->assertIdentical($this->security->getDescription(), $this->DESCRIPTION);
+		$this->assertIdentical($this->security->getIsDefault(),   $this->ISDEFAULT);
+		$this->assertIdentical($this->security->getcanEditOther(), $this->CANEDITOTHER);
+		$this->assertIdentical($this->security->canPromote(), $this->CANPROMOTE);
+		$this->assertIdentical($this->security->siteAdmin(), $this->SITEADMIN);
 	}
 
-	// test updating a User in mySQL
-	public function testUpdateUser() {
-		// first, verify mySQL connected OK
-		$this->assertNotNull($this->mysqli);
-
-		// second, create a user to post to mySQL
-		$this->user = new User(null, $this->EMAIL, $this->HASH, $this->SALT, $this->AUTH_TOKEN);
-
-		// third, insert the user to mySQL
-		$this->user->insert($this->mysqli);
-
-		// fourth, update the user and post the changes to mySQL
-		$newEmail = "jake@cortez.org.mx";
-		$this->user->setEmail($newEmail);
-		$this->user->update($this->mysqli);
-
-		// finally, compare the fields
-		$this->assertNotNull($this->user->getUserId());
-		$this->assertTrue($this->user->getUserId() > 0);
-		$this->assertIdentical($this->user->getEmail(),               $newEmail);
-		$this->assertIdentical($this->user->getPassword(),            $this->HASH);
-		$this->assertIdentical($this->user->getSalt(),                $this->SALT);
-		$this->assertIdentical($this->user->getAuthenticationToken(), $this->AUTH_TOKEN);
-	}
-
-	// test deleting a User
-	public function testDeleteUser() {
-		// first, verify mySQL connected OK
-		$this->assertNotNull($this->mysqli);
-
-		// second, create a user to post to mySQL
-		$this->user = new User(null, $this->EMAIL, $this->HASH, $this->SALT, $this->AUTH_TOKEN);
-
-		// third, insert the user to mySQL
-		$this->user->insert($this->mysqli);
-
-		// fourth, verify the User was inserted
-		$this->assertNotNull($this->user->getUserId());
-		$this->assertTrue($this->user->getUserId() > 0);
-
-		// fifth, delete the user
-		$this->user->delete($this->mysqli);
-		$this->user = null;
-
-		// finally, try to get the user and assert we didn't get a thing
-		$hopefulUser = User::getUserByEmail($this->mysqli, $this->EMAIL);
-		$this->assertNull($hopefulUser);
-	}
-
-	// test grabbing a User from mySQL
-	public function testGetUserByEmail() {
-		// first, verify mySQL connected OK
-		$this->assertNotNull($this->mysqli);
-
-		// second, create a user to post to mySQL
-		$this->user = new User(null, $this->EMAIL, $this->HASH, $this->SALT, $this->AUTH_TOKEN);
-
-		// third, insert the user to mySQL
-		$this->user->insert($this->mysqli);
-
-		// fourth, get the user using the static method
-		$staticUser = User::getUserByEmail($this->mysqli, $this->EMAIL);
-
-		// finally, compare the fields
-		$this->assertNotNull($staticUser->getUserId());
-		$this->assertTrue($staticUser->getUserId() > 0);
-		$this->assertIdentical($staticUser->getUserId(),              $this->user->getUserId());
-		$this->assertIdentical($staticUser->getEmail(),               $this->EMAIL);
-		$this->assertIdentical($staticUser->getPassword(),            $this->HASH);
-		$this->assertIdentical($staticUser->getSalt(),                $this->SALT);
-		$this->assertIdentical($staticUser->getAuthenticationToken(), $this->AUTH_TOKEN);
-	}
-}
-?>
