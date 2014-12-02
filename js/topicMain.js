fd856246e6b2cef7f41f38498b0843e52108c448
Marc Hayes <marc.hayes.tech@gmail.com>
2014-12-02 00:17:29 -0700

    basic setup for a topic page works now. values are hard coded into js for link and will need ot be cleaned up when i can follow up on how to pass variables into js from html url
    https://app.asana.com/0/19128794071969/20811486365154

--- js/topicMain.js
+++ js/topicMain.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 	$.ajax({
+		// FIXME: fix the hard coded values below
 		url    : '../php/form/topicMainTopic.php?t=2',
 		success: function(ajaxOutput) {
 			$("#topic").html(ajaxOutput);
 		}
 	});
 	$.ajax({
+		// FIXME: fix the hard coded values below
 		url    : '../php/form/topicMainComments.php?t=2',
 		success: function(ajaxOutput) {
 			$("#comments").html(ajaxOutput);
