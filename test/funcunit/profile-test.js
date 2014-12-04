d288a05fa5aba933f8b3b1dcaa088e714a17baef
Dylan McDonald <dmcdonald21@cnm.edu>
2014-12-04 09:36:29 -0700

    Completed a functional unit test https://app.asana.com/0/19248945971906/20811486365150

--- test/funcunit/profile-test.js
+++ test/funcunit/profile-test.js
@@ -12,6 +12,7 @@ var VALID_LASTNAME    = "Simpson";
 var VALID_LOCATION    = "Springfield, NT";
 var VALID_DESCRIPTION = "Leader of the Stone Cutters";
 
+// define a function to perform the actual unit tests
 function testValidFields() {
 	// fill in the form values
 	F("#firstName").visible(function() {
@@ -29,6 +30,20 @@ function testValidFields() {
 	F("#description").visible(function() {
 		this.type(VALID_DESCRIPTION);
 	});
+
+	// click the button once all the fields are filled in
+	F("#profileSubmit").visible(function() {
+		this.click();
+	});
+
+	// in forms, we want to assert the form worked as expected
+	// here, we assert we got the success message from the AJAX call
+	F(".alert").visible(function() {
+		// the ok() function from qunit is equivalent to SimpleTest's assertTrue()
+		ok(F(this).hasClass("alert-success"), "successful alert CSS");
+		ok(F(this).html().indexOf("Updated Successful") >= 0, "successful message");
+	});
 }
 
+// the test function *MUST* be called in order for the test to execute
 test("test valid fields", testValidFields);
