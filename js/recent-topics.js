c65c697111271674b7ee52aba282f6e89918a052
Marc Hayes <marc.hayes.tech@gmail.com>
2014-12-04 15:26:54 -0700

    Combined all assoc files for recent view into a single PHP file with the same functionality to simplify the process and better support non-javascript browsers.
    https://app.asana.com/0/19128794071969/20811486365154
    https://app.asana.com/0/19128794071969/20811486365156

--- js/recent-topics.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-	$.ajax({
-		url    : 'php/form-processor/recent-topics.php',
-		success: function(ajaxOutput) {
-			$("#recent").html(ajaxOutput);
-		}
-	});
-});
--- recent.php
+++ recent.php
@@ -1,6 +1,8 @@
 <?php
-require_once("php/lib/csrf.php");
 session_start();
+require_once("php/lib/csrf.php");
+require_once("/etc/apache2/capstone-mysql/ddconnect.php");
+require_once("php/class/topic.php");
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,21 +16,39 @@ session_start();
 		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
 		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="js/recent-topics.js"></script>
+<!--		<script type="text/javascript" src="js/recent-topics.js"></script>-->
 	</head>
 	<body>
-		<div class="container">
-			<div class="row">
-				<div id="recent"></div>
-			</div>
-			<div class="row">
-				<div>
-					<form action="topic-newedit.php">
-						<?php echo generateInputTags(); ?>
-						<input type="submit" value="Create a Topic">
-					</form>
-				</div>
-			</div>
-		</div>
+		<article class="container">
+			<section class="row">
+				<?php
+				try {
+					$mysqli = MysqliConfiguration::getMysqli();
+
+					// grab topic from database
+					$topics = Topic::getRecentTopics($mysqli, 100000);
+
+					// make sure it exists
+					if ($topics !== null) {
+						// iterate down array and prep html
+						foreach($topics as $index => $element) {
+							echo	"<p><a href=\"topic.php?t=" . $element->getTopicId() . "\">" . $element->getTopicSubject() . "</a></p>" .
+								"<p>" . substr($element->getTopicBody(), 0, 100) . "...</p>";
+						}
+					} else {
+						echo "<h4>No topics currently exist.</h4>";
+					}
+
+				} catch(Exception $exception) {
+					echo "<div class=\"alert alert-danger\" role=\"alert\">Unable to load recent topics: " . $exception->getMessage() . "</div>";
+				}
+				?>
+			</section>
+			<section class="row">
+				<form action="topic-newedit.php">
+					<input type="submit" value="Create a Topic">
+				</form>
+			</section>
+		</article>
 	</body>
 </html>
