8d280dbe417e3ed680687dceb5a00aeab8671f5a
Steven-Chavez <schavez256@yahoo.com>
2014-12-08 21:12:06 -0700

    Added cohort-edit page and stub. Wrote stub so that options can grow as the cohorts do. https://app.asana.com/0/19128794071859/20811486365150

---
+++ php/stub/cohort-edit-stub.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Steven
+ * Date: 12/8/2014
+ * Time: 11:53 AM
+ */
+require_once("php/class/cohort.php");
+
+//declare variables
+//$cohort[] = array();
+$cohortBool = true;
+$id = 1;
+
+//catch all the cohort objects in $cohort array
+while($cohortBool){
+if(($cohort[] = Cohort::getCohortByCohortId($mysqli, $id)) !== null){
+		$id++;
+	}
+	else{
+		$cohortBool = false;
+	}
+}
+
+//get total number of elements in array
+$total = count($cohort);
+
+
+
+echo "<h3>Cohort Edit</h3>
+		<form id=\"cohortEditForm\" action=\"php/form-processor/cohort-edit-processor.php\" method=\"POST\">
+			<select id=\"cohortOption\" name=\"cohortOption\">
+				<option value=\"null\">pick your cohort...</option>";
+
+for($i = 0; $i < $total-1; $i++) {
+	$startDate = $cohort[$i][0]->getStartDate();
+	$endDate = $cohort[$i][0]->getEndDate();
+
+	echo "<option value=\"" . $id . "\">";
+	echo $startDate->format("M Y") . " - " . $endDate->format("M Y") . "<br>";
+	echo "</option>";
+}
+
+echo "	</select>
+	<button id=\"profileSubmit\" type=\"submit\" name=\"submit\">Submit</button>
+</form>
+<p id=\"CohortEditOutput\"></p>";
