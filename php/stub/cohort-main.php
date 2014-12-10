ea1c2df3eedbbdf05751e482636f6170a612a5b9
Gerry Medrano <lux808@hotmail.com>
2014-12-10 14:10:45 -0700

    https://app.asana.com/0/19129992621202/20811486365158 cohort-main arranged div classes and edit cohort pull

--- php/stub/cohort-main.php
+++ php/stub/cohort-main.php
@@ -1,19 +1,29 @@
 <?php
+
+//calls the databases
 require_once("/etc/apache2/capstone-mysql/ddconnect.php");
 require_once("php/class/cohort.php");
 
+//gets MySqli information
 $mysqli = MysqliConfiguration::getMysqli();
-
 $cohorts = Cohort::getCohorts($mysqli);
 
 
+
+//Output to user
 echo "<div class=\"row\">";
 foreach($cohorts as $index => $element) {
    echo "<a href=\"cohort.php?cohort=" . $element->getCohortId() . "\" class=\"col-xs-4\">" . $element->getDescription() . "</p>";
-
+   echo "<a href=\"cohort.php?cohort=" . $element->getCohortId() . "\" class=\"col-xs-4\">" . $element->getStartDate()->format("M Y") . "</p>";
+   echo "<a href=\"cohort.php?cohort=" . $element->getCohortId() . "\" class=\"col-xs-4\">" . $element->getLocation() . "</p>";
 }
 
-echo "</div>";
+echo "
+
+
+<div>
+Test page for Cohorts
+</div>";
 
 
 
