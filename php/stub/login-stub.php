5503dc3c4c6e2e55bdc0147a49f86f1284e547f4
Marc-Hayes <marc.hayes.tech@gmail.com>
2014-12-13 14:21:06 -0700

    cleaned up login form formatting on mobile and browser

--- php/stub/login-stub.php
+++ php/stub/login-stub.php
@@ -2,18 +2,19 @@
 
 echo "<aside class=\"col-sm-3 col-xs-12\">
 	<form name=\"login\" action=\"php/form-processor/login-form-processor.php\" method=\"POST\" accept-charset=\"utf-8\">
-	<div class=\"col-sm-12 hidden-xs\">
+	<div class=\"col-sm-12 hidden-xs input-group input-group-xs\">
 		<h4><strong>Login</strong></h4>
 	</div>
 	<div class=\"col-sm-12 col-xs-4\">
-		<input type=\"email\" name=\"email\" placeholder=\"yourname@email.com\" required>
+		<input type=\"email\" class=\"form-control\" name=\"email\" placeholder=\"yourname@email.com\" required>
 	</div>
 	<div class=\"col-sm-12 col-xs-4\">
-		<input type=\"password\" name=\"password\" placeholder=\"password\" required>
+		<input type=\"password\" class=\"form-control\" name=\"password\" placeholder=\"password\" required>
 	</div>
 	<div class=\"col-sm-12 col-xs-4\">
-		<button id=\"loginBtn\" type=\"submit\" class=\"btn btn-primary btn-xs\" name=\"submit\">Login</button>
-		<button class=\"btn btn-primary btn-xs\" onclick=\"signupForm.php\">Sign-up</a>
+		<button id=\"loginBtn\" type=\"submit\" class=\"btn btn-primary btn-sm btn-sm\" name=\"submit\">Login</button>
+		<button class=\"btn btn-primary btn-sm\" onclick=\"signupForm.php\">Sign-up</a>
+	</div>
 	</div>
 	</form>
 </aside>";
