4e6e376fc998d4923fb31fb19d5a351e188ddc48
Gerry Medrano <lux808@hotmail.com>
2014-12-15 13:56:07 -0700

    added basic text for footer - stub

--- php/stub/footer.php
+++ php/stub/footer.php
@@ -1,7 +1,10 @@
 <?php
 
-echo			"</article>
-			</section><!-- row -->
-		</main>
-	</body>
-</html>";
+   echo
+   "<div class = footer>
+		<p>DEEP DIVE CONNECT is a social forum created by, and for, Deep Dive Coders (past, present and future) and their friends.</p>
+      <p>For more information on Deep Dive Coders at the Stemulus Center in downtown Albuquerque, visit us at</p></br>
+      <p>http://stemuluscenter.org/products/deep-dive-coding-bootcamp</p>
+   </div>";
+
+?>
