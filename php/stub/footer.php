f07d17b55e450f1cd3e5175c07634a6633d74881
Steven-Chavez <schavez256@yahoo.com>
2014-12-15 20:05:09 -0700

    Updated footer along with css to make the footer more presentable

--- php/stub/footer.php
+++ php/stub/footer.php
@@ -1,10 +1,16 @@
 <?php
-   //add some comments for footer
-   echo
-   "<div class = footer>
+  //add some comments for footer
+echo			"</article>
+			</section><!-- row -->";
+
+echo
+"		<footer class = \"row\">
 		<p>DEEP DIVE CONNECT is a social forum created by, and for, Deep Dive Coders (past, present and future) and their friends.</p>
       <p>For more information on Deep Dive Coders at the Stemulus Center in downtown Albuquerque, visit us at</p></br>
       <p>http://stemuluscenter.org/products/deep-dive-coding-bootcamp</p>
-   </div>";
+   		</footer>";
 
-?>
+echo	"</main>
+	</body>
+</html>";
+
