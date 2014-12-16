f70cbf44373db5d1d56b4ea584f5afc8903e75b3
Marc-Hayes <marc.hayes.tech@gmail.com>
2014-12-15 21:39:19 -0700

    update to footer to clean up the last bit of formatting

--- php/stub/footer.php
+++ php/stub/footer.php
@@ -3,14 +3,14 @@
 echo			"</article>
 			</section><!-- row -->";
 
-echo
-"		<footer class = \"row\">
-		<p>DEEP DIVE CONNECT is a social forum created by, and for, Deep Dive Coders (past, present and future) and their friends.</p>
-      <p>For more information on Deep Dive Coders at the Stemulus Center in downtown Albuquerque, visit us at</p></br>
-      <p>http://stemuluscenter.org/products/deep-dive-coding-bootcamp</p>
-   		</footer>";
 
 echo	"</main>
+		<footer class = \"container\">
+			<div class=\"col-xs-offset-1 col-xs-10\">
+				<p>DEEP DIVE CONNECT is a social forum created by, and for, Deep Dive Coders (past, present and future) and their friends.<br>
+      		For more information on Deep Dive Coders at the Stemulus Center in downtown Albuquerque, visit us at<br>
+      		<a href=\"http://stemuluscenter.org/products/deep-dive-coding-bootcamp\">http://stemuluscenter.org/products/deep-dive-coding-bootcamp</a></p>
+      	</div>
+   	</footer>
 	</body>
 </html>";
-
