bb06b8d763046ac6281bbae1f394d1de33e6c2e2
Marc-Hayes <marc.hayes.tech@gmail.com>
2014-12-15 21:37:08 -0700

    Updated status message library to store in session to prevent POST values going out of scope from page to page.

--- php/lib/status-message.php
+++ php/lib/status-message.php
@@ -3,7 +3,7 @@
 // getStatusMessage("profile-edit");
 function getStatusMessage($form) {
 	$message = null;
-	$postArray = filter_input(INPUT_POST, $form, FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);
+	$postArray = isset($_SESSION[$form]) ? $_SESSION[$form] : false;
 	if (is_array($postArray) !== false) {
 		if (array_key_exists('fail', $postArray)) {
 			$message = filter_var($postArray['fail'], FILTER_SANITIZE_STRING);
@@ -13,10 +13,11 @@ function getStatusMessage($form) {
 			$message = "<div class=\"alert alert-success\" role=\"alert\"><p>$message</p></div>";
 		}
 	}
+	unset($_SESSION[$form]);
 	return($message);
 }
 
 // setStatusMessage("profile-edit","fail","MESSAGE GOES HERE");
 function setStatusMessage($form, $status, $message){
-	$_POST[$form][$status] = $message;
+	$_SESSION[$form][$status] = $message;
 }
