2b5ab7a71ac179239e2a682d0fa0e1b05f59a9e0
Marc Hayes <marc.hayes.tech@gmail.com>
2014-12-17 15:04:44 -0700

    fixed error during signup link

--- php/stub/signup-stub.php
+++ php/stub/signup-stub.php
@@ -2,12 +2,13 @@
 /**
  * Created in collaboration by:
  *
- * Gerardo Medrano GMedranoCode@gmail.com
+ * Gerardo Medrano <GMedranoCode@gmail.com>
  * Marc Hayes <Marc.Hayes.Tech@gmail.com>
  * Steven Chavez <schavez256@yahoo.com>
- * Joseph Bottone hi@oofolio.com
+ * Joseph Bottone <hi@oofolio.com>
  *
  */
+
 require_once("php/lib/csrf.php");
 
 
